473524060a44fe95ce1ae75d2533708e2c7f42b3
Hammam Hussein <hamamhussein10@gmail.com>
2026-02-07 21:47:12 +0300

    Improve hooks

--- src/hooks/use-review.ts
+++ src/hooks/use-review.ts
@@ -1,124 +1,78 @@
-import {
-  addReview,
-  getReviewsForProduct,
-  removeReview,
-  updateReview,
-} from '@/api/Reviews';
+import { Reviewscommand } from '@/features/reviews/command/reviews.command';
+import { ReviewsQuery } from '@/features/reviews/query/reviews.query';
+import useReviewFormState from '@/stores/reviews/useReviewFormState';
+import { useReviewStore } from '@/stores/reviews/useReviewsStore';
 import { useUserStore } from '@/stores/useUserStore';
-import type { ProductReviewsProps, Review } from '@/types/product';
-import handleError from '@/utils/ErrorHandler';
-import { useMutation, useQuery } from '@tanstack/react-query';
-import { useEffect, useState } from 'react';
+import type { ProductReviewsProps } from '@/types/product';
+import { useEffect } from 'react';
 
-const useReview = ({ onAddReview, productId }: ProductReviewsProps) => {
-  const [newRating, setNewRating] = useState(0);
-  const [newComment, setNewComment] = useState('');
-  const [hoveredRating, setHoveredRating] = useState(0);
-  const [isEdit, setIsEdit] = useState(false);
-  const [reviewId, setReviewId] = useState<string | null>(null);
-
+const useReview = ({ productId }: ProductReviewsProps) => {
   const user = useUserStore((state) => state.user);
 
+  const setReviews = useReviewStore((state) => state.setReviews);
+  const { form, setComment, setHoveringRate, setRate, resetForm, setForm } =
+    useReviewFormState();
   const {
-    data: reviews,
-    isLoading,
-    refetch,
-  } = useQuery<{ data: Review[] }, Error, Review[]>({
-    queryKey: ['getReviews', productId],
-    queryFn: () => getReviewsForProduct(productId),
-    select: (response) => response.data,
-  });
-
-  const { mutate: AddReviewMutation } = useMutation({
-    mutationKey: ['add-review'],
-    mutationFn: addReview,
-    onSuccess: () => {
-      refetch();
-    },
-    onError: (err) => handleError(err, 'reviews'),
-  });
-
-  const { mutate: updateReviewMutation } = useMutation({
-    mutationKey: ['update-review'],
-    mutationFn: updateReview,
-    onSuccess: (data) => {
-      refetch();
-      console.log('Review Added successfully');
-      console.log(data);
-    },
-    onError: (err) => {
-      console.log(err);
-    },
-  });
-
-  const { mutate: removeReviewMutation } = useMutation({
-    mutationKey: ['remove-review'],
-    mutationFn: removeReview,
-    onSuccess: (data) => {
-      refetch();
-      console.log('Review Added successfully');
-      console.log(data);
-    },
-    onError: (err) => {
-      console.log(err);
-    },
-  });
+    getReviewsQuery: { data: reviews, refetch, isLoading },
+  } = ReviewsQuery(productId);
 
   useEffect(() => {
-    if (!isLoading) console.log('Reviews ', reviews);
-  }, [reviews]);
+    if (reviews?.length) setReviews(reviews);
+  }, [reviews, isLoading]);
+
+  const {
+    AddReviewCommand: { mutate: AddReviewMutation },
+    removeReviewCommand: { mutate: removeReviewMutation },
+    updateRevieCommand: { mutate: updateReviewMutation },
+  } = Reviewscommand({ refetch });
 
   const handleSubmitReview = () => {
-    if (newRating > 0 && newComment.trim()) {
-      onAddReview(newRating, newComment.trim());
-      if (isEdit) {
-        updateReviewMutation({
-          comment: newComment.trim(),
-          rating: newRating,
-          _id: reviewId!,
-        });
-        setIsEdit(false);
-      } else {
-        AddReviewMutation({
-          user: {
-            username: user!.username,
-            profileImg: user!.profileImg,
-            _id: user!._id,
-          },
-          rating: newRating,
-          comment: newComment.trim(),
-          product: productId,
-        });
-      }
-      setNewRating(0);
-      setNewComment('');
+    if (!form.comment.trim() || form.rate == 0) return;
+    if (form.mode == 'edit' && form.reviewId) {
+      updateReviewMutation({
+        comment: form.comment.trim(),
+        rating: form.rate,
+        _id: form.reviewId,
+      });
+    } else {
+      AddReviewMutation({
+        user: {
+          username: user!.username,
+          profileImg: user!.profileImg,
+          _id: user!._id,
+        },
+        rating: form.rate,
+        comment: form.comment.trim(),
+        product: productId,
+      });
     }
+    resetForm();
   };
 
   const handleOnEdit = (reviewId: string) => {
-    setIsEdit(true);
-    setReviewId(reviewId);
-    const review = reviews?.find((review) => review._id === reviewId);
-    if (!review) {
-      setIsEdit(false);
-      return;
-    }
-    setNewComment(review.comment);
-    setNewRating(review.rating);
+    const review = reviews?.find((r) => r._id === reviewId);
+
+    if (!review) return;
+
+    setForm({
+      mode: 'edit',
+      reviewId,
+      rate: review.rating,
+      comment: review.comment,
+      hoverRating: 0,
+    });
   };
 
   return {
-    newRating,
-    setNewRating,
-    newComment,
-    setNewComment,
-    hoveredRating,
-    setHoveredRating,
+    form,
     handleOnEdit,
     handleSubmitReview,
     removeReviewMutation,
     reviews,
     user,
+    setComment,
+    setRate,
+    setHoveringRate,
   };
 };
 
