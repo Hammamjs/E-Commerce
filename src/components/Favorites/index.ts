0db16fa03d6aaac374b4852ed512918ba2f34b1f
Hammam Hussein <hamamhussein10@gmail.com>
2026-02-17 07:21:33 +0300

    Track deleted files

--- src/components/Favorites/index.ts
+++
@@ -1,9 +0,0 @@
-// Main Component
-export { default as Favorites } from './Favorites';
-
-// Sub Components
-export { default as FavoriteHeader } from './FavoriteHeader';
-export { default as FavoriteNotFound } from './FavoriteNotFound';
-export { default as FavoritesList } from './FavoritesList';
-export { default as ProductTemplate } from './ProductTemplates';
-export { default as QuickAction } from './QuickAction';
--- src/utils/capitalize.ts
+++
@@ -1,1 +0,0 @@
-export const capitalize = (s: string) => s.charAt(0) + s.slice(1);
